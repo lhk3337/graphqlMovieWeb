3cfefe5673fd780f9c4be0272a83a28377e709a4
lhk3337 <lhk3337@naver.com>
2021-06-28 14:18:03 +0900

    rest API를 graphQL로 변환

--- server/graphql/db.js
+++ server/graphql/db.js
@@ -1,36 +1,7 @@
-let movies = [
-  { id: 0, name: "holim", score: 20 },
-  { id: 1, name: "Alex", score: 30 },
-  { id: 2, name: "Lindsay", score: 30 },
-  { id: 3, name: "Nathan", score: 25 },
-  { id: 4, name: "Christine", score: 30 },
-  { id: 5, name: "Kate", score: 24 },
-];
+import fetch from "node-fetch";
+const API_URL = "https://yts.mx/api/v2/list_movies.json";
 
-export const getMovies = () => movies;
-
-export const getById = (id) => {
-  const filterMovies = movies.filter((movie) => id === movie.id);
-  return filterMovies[0];
-};
-
-export const deleteMovie = (id) => {
-  const cleanMovies = movies.filter((movie) => id !== movie.id);
-  if (movies.length > cleanMovies.length) {
-    movies = cleanMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  console.log(newMovie);
-  return newMovie;
-};
+export const getMovies = (limit, lating) =>
+  fetch(API_URL)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
